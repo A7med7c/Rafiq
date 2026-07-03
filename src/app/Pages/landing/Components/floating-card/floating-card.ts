487073db23830573eb8df658e5a55c5e161e5221
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-07-03 04:17:40 +0300

    test: add floating card component scaffold

---
+++ src/app/Pages/landing/Components/floating-card/floating-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-card',
+  imports: [],
+  templateUrl: './floating-card.html',
+  styleUrl: './floating-card.css',
+})
+export class FloatingCard {}
